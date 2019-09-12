b9a203a04089865c5b6d112188dc270a3e672e01
viddhamo <vidhyasagar.dhamo@gmail.com>
2019-09-12 15:15:39 +1000

    minor updates

--- Python_App_That_Tracks_Amazon_Prices.py
+++ Python_App_That_Tracks_Amazon_Prices.py
@@ -10,6 +10,7 @@ import smtplib
 URL = 'https://www.amazon.com.au/Ingenuity-InLighten-Cradling-Rocker-Unicorn/dp/B07NPLRQRG/ref=sr_1_fkmr0_2?keywords=Ingenuity+SimpleComfort+Cradling+Swing+Everston&qid=1568091870&s=gateway&sr=8-2-fkmr0'
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
 
+# Function to check the price of URL and if condition satisfies calls the send email method
 def check_price():
     # Download the webpage content and initialise the BeautifulSoup html parser
     page = req.get(URL, headers=headers)
@@ -19,12 +20,13 @@ def check_price():
     #price = soup.find('div',{'class':"price-only"}).get_text()
     #price = soup.find('div',{'class':"current-price"})
     price = soup.find('span',attrs={'id':'priceblock_ourprice'}).get_text()
+    print(price)
     converted_price = float(price[1:7])
 
     if(converted_price < 259.0):
         send_mail()
 
-
+# Function to send email alert using gmail app passowords
 def send_mail():
     print("entered into send mail function")
     server = smtplib.SMTP('smtp.gmail.com', 587)
